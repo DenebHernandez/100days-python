ce09a3dda24e2eaa8b2830bc48cc996bc96e183c
Deneb Hernandez <denebhdz@gmail.com>
2022-09-28 19:14:42 -0600

    day one challenge

---
+++ day1_band_name_generator.py
@@ -0,0 +1,10 @@
+
+print("Welcome to the band name generator!")
+
+city = input("What's the name of the city you grew up in? \n")
+
+pet = input("What's your pet's name \n")
+
+band_name= f"{city} {pet}"
+
+print(f"Your band name could be {band_name}")
