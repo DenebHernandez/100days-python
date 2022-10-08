ef9354d179d83c998be10a7c0dc47fdec97a687f
Deneb Hernandez <denebhdz@gmail.com>
2022-10-08 08:51:33 -0600

    finnished day 9 challenge

--- A-Begginer/day9_blind_auction.py
+++ A-Begginer/day9_blind_auction.py
@@ -4,3 +4,31 @@ import os
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
+def find_highest_bid(bid_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bid_record:
+        bid_amount = bid_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+print(logo)
+
+bids = {}
+end_auction = False
+
+while not end_auction:
+    name = input("What is your name?\n")
+    bid = int(input("What is your bid?\n"))
+
+    bids[name] = bid
+
+    should_continue = input("Are there any other bidders? Type yes or no:\n")
+    if should_continue == "no":
+        end_auction = True
+    cls()
+
+find_highest_bid(bids)
