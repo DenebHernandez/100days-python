1d3ca2df3d2f4d0262bfb43b8cb327c2c37c2f7c
Deneb Hernandez <denebhdz@gmail.com>
2022-10-24 17:24:11 -0600

    fixed indentation issue and unused import

--- B-Intermediate/day16/main.py
+++ B-Intermediate/day16/main.py
@@ -1,4 +1,3 @@
-from secrets import choice
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
@@ -20,4 +19,4 @@ while is_on:
     else:
         drink = menu.find_drink(order)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-                coffee_maker.make_coffee(drink)
+            coffee_maker.make_coffee(drink)
