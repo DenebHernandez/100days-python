ddf12a00997dc4167ed6805808cedb43986b94e2
DenebHernandez <denebhdz@gmail.com>
2022-10-26 21:08:55 -0600

    initial day 18 commit

---
+++ B-Intermediate/day18/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+timmy.right(90)
+timmy.forward(100)
+
+screen = Screen()
+screen.exitonclick()
