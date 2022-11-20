2635e0778914f23669fa5015105776e6c13f8484
DenebHernandez <denebhdz@gmail.com>
2022-11-19 20:27:23 -0600

    nato alphabet generator

--- B-Intermediate/day26/main.py
+++ B-Intermediate/day26/main.py
@@ -1,27 +1,17 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+import os
+import pandas as pd
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+main_folder = os.getcwd()
+cwd = f"{main_folder}\B-Intermediate\day26"
 
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+df = pd.read_csv(f"{cwd}\\nato_phonetic_alphabet.csv")
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# Create a dictionary from the dataframe
+nato_dict = {row.letter:row.code for (index, row) in df.iterrows()}
+# print(nato_dict)
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+word = input("Enter a word").upper()
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+#Create a list of the phonetic code words from a word that the user inputs.
+result = [nato_dict[letter] for letter in word]
+print(result)
