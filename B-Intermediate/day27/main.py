7eb51478135f1355ed74ea58909c3ff66637c8e7
DenebHernandez <denebhdz@gmail.com>
2022-11-20 17:20:45 -0600

    final day 27 commit

--- B-Intermediate/day27/main.py
+++ B-Intermediate/day27/main.py
@@ -1,7 +1,31 @@
 import tkinter as tk
 
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    kilometer_result_label.config(text=f"{km}")
+
 window = tk.Tk()
-window.title(string="Test")
-window.minsize(width=400, height=400)
+window.title(string="Miles to Kilometer Converter")
+# window.minsize(width=400, height=400)
+window.config(padx=20, pady=20)
+
+miles_input = tk.Entry(width=7)
+miles_input.grid(column=1 , row=0)
+
+miles_label = tk.Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = tk.Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilometer_result_label = tk.Label(text="")
+kilometer_result_label.grid(column=1, row=1)
+
+kilometer_label = tk.Label(text="Km")
+kilometer_label.grid(column=2, row=1)
+
+calculate_button = tk.Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1 , row=2)
 
 window.mainloop()
