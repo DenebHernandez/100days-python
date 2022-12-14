938e2ec7b1bbf94042768933ad61af3b8452e854
Deneb Hernandez <denebhdz@gmail.com>
2022-12-13 18:44:15 -0600

    add flip card method

--- B-Intermediate/day31/main.py
+++ B-Intermediate/day31/main.py
@@ -6,17 +6,25 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
     current_word = current_card["French"]
     canvas.itemconfig(card_title, text="French")
     canvas.itemconfig(card_word, text=current_word)
 
+def flip_card():
+    current_word = current_card["English"]
+    canvas.config(card_title, text="English")
+    canvas.config(card_word, text=current_word)
 
 window = tk.Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+window.after(3000, flip_card)
 
 canvas = tk.Canvas(width=800, height=526)
 card_front_img = tk.PhotoImage(file="images/card_front.png")
